bcd26fd80c7c601d41f48e57c0ca958d1bfa366e
pavan.kumar9 <pavan.kumar@freighttiger.com>
2022-06-27 22:28:25 +0530

    updated to consume secrets dynamically from list of maps

--- module/main.tf
+++ module/main.tf
@@ -1,15 +1,22 @@
+locals {
+  encrypted_secrets = yamldecode(file(var.encrypted_secrets_file_path))  
+}
+
 data "aws_kms_secrets" "secret" {
-  secret {
-    name    = "secret"
-    payload = file(var.encrypted_file_path)
+  dynamic secret {
+    for_each = var.parameters
+    content {
+      name    = secret.value.key // 1. holds the yaml key ex: PASS1
+      payload = local.encrypted_secrets[secret.value.name] // 2. holds the yaml value for the key PASS1 ex: AQ....
+    }
   }
 }
 
 resource "aws_ssm_parameter" "parameter" {
-  name        = var.name
+  for_each    = { for parameter in var.parameters : parameter.name => parameter }
+  name        = each.value.key 
+  value       = data.aws_kms_secrets.secret.plaintext[each.value.key] // decrypted value of (2)
   description = var.description
   type        = "SecureString"
-  value       = data.aws_kms_secrets.secret.plaintext["secret"]
   tags        = var.tags
-  depends_on = [data.aws_kms_secrets.secret]
 }
--- module/variables.tf
+++ module/variables.tf
@@ -3,19 +3,17 @@ variable "tags" {
   type        = map(string)
   default     = {}
 }
-
-variable "name" {
-  description = "name of the SSM parameter key eg: /test/MY_DB_PASSWORD"
-  type = string
-}
-
 variable "description" {
   description = "key name description"
   type = string
   default = ""
 }
 
-variable "encrypted_file_path" {
+variable "encrypted_secrets_file_path" {
   description = "file path containing the cipher blob of the plain text value"
   type = string
+}
+
+variable "parameters" {
+  description = "file path containing the cipher blob of the plain text value"
 }
