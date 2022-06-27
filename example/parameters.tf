895c5d884118b925f5e86bd9f6c99dad81f401dc
pavan.kumar9 <pavan.kumar@freighttiger.com>
2022-06-27 22:37:25 +0530

    update comments

--- example/parameters.tf
+++ example/parameters.tf
@@ -1,10 +1,10 @@
 module "example-service" {
-  source              = "../module"
-  encrypted_secrets_file_path = "./example-service-secrets.yaml.encrypted"
+  source                      = "../module"
+  encrypted_secrets_file_path = "./example-secrets.yaml.encrypted"
   parameters = [
     {
-      key  = "/test/myservice/PASS1"
-      name = "PASS1"
+      key  = "/test/myservice/PASS1" // name of the parameter 
+      name = "PASS1"                 // yaml key as in "./example-secrets.yaml.encrypted" that holds encrypted value
     },
     {
       key  = "/test/myservice/PASS2"
